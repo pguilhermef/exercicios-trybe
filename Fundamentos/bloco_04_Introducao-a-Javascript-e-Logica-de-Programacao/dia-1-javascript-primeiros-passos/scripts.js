1c3cc69e9c54ea43009d593a0eada98716dfa7b4
pguilhermef <pguilhermesantos@hotmail.com>
2022-05-12 18:30:10 -0300

    Finalização do Exercício

---
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,9 @@
+const myName = "Paulo Guilherme"
+const birthCity = "Rio de Janeiro"
+let birthYear = 2003
+
+birthYear = 2030
+
+// console.log(myName)
+console.log(birthCity)
+// console.log(birthYear)
