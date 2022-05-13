3eaad7ce60001c4777f87b8b10b08211d51da3ef
pguilhermef <pguilhermesantos@hotmail.com>
2022-05-13 02:23:03 -0300

    Adição exercício 1 e 2

---
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/exercício/1.js
@@ -0,0 +1,8 @@
+const a = 3
+const b = 10
+
+console.log("O resultado da soma é de:", (a+b))
+console.log("O resultado da subtração é de:", (a-b))
+console.log("O resultado da divisão é de:", (a/b))
+console.log("O resultado da multiplicação é de:", (a*b))
+console.log("O resultado da exponeciação é de:", (a**b))
---
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/exercício/2.js
@@ -0,0 +1,7 @@
+const a = 9
+const b = 10
+
+if (a > b) {
+  console.log("A variável A é maior, pois possui valor igual a", a, "enquanto B tem valor igual a", b)
+} else {
+  console.log("A variável B é maior, pois possui valor igual a", b, "enquanto A tem valor igual a", a)
