ffe8a86ffc500b01bc186fcf2528b9218c86032f
pguilhermef <pguilhermesantos@hotmail.com>
2022-05-13 02:31:16 -0300

    Exercício 3 completo

---
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/exercício/3.js
@@ -0,0 +1,11 @@
+const a = 11
+const b = 6
+const c = 3
+
+if (a > b && a > c) {
+  console.log("A variável A é a maior.")
+} else if (b > a && b > c) {
+  console.log("A variável B é a maior.")
+} else {
+  console.log("A variável C é a maior.")
+}
