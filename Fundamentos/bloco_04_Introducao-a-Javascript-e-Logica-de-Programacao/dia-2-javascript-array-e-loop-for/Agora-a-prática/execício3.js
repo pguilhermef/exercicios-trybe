6a7978dd56462cb1b9583ef8a7c1be98e192a740
pguilhermef <pguilhermesantos@hotmail.com>
2022-05-13 19:09:32 -0300

    Finalização de terceiro exercício

--- Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/Agora-a-prática/execício3.js
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/Agora-a-prática/execício3.js
@@ -1,1 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0
+
+for (let index = 0; index < numbers.length; index ++){
+  soma += numbers[index]
+}
+
+let media = soma / 10
+
+console.log(media)
