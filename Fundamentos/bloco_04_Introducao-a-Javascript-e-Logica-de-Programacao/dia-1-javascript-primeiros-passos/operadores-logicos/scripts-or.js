b51702cb16022675eebd0735c55c86b94e15a5e8
pguilhermef <pguilhermesantos@hotmail.com>
2022-05-13 01:26:50 -0300

    Adição e finalização do quinto exercício

---
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/operadores-logicos/scripts-or.js
@@ -0,0 +1,9 @@
+let weekDay = "segunda-feira"
+
+// Se nossa variável "weekDay" for "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira" ou "sexta-feira", imprima "Oba, mais um dia de aprendizado na Trybe >:D".
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira!" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+} else {
+  console.log("FINALMENTE, descanso merecido UwU")
+}
