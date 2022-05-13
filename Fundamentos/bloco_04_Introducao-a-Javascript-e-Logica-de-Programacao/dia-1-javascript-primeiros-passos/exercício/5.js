edd77e4a5297de8d7f62a6320c1dbb940cc41b10
pguilhermef <pguilhermesantos@hotmail.com>
2022-05-13 03:07:23 -0300

    Alteração do código de condição e renomeamento de variaveis

--- Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/exercício/5.js
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-1-javascript-primeiros-passos/exercício/5.js
@@ -1,31 +1,9 @@
-const a = 6
-const b = 12
-const c = 5
+const lado1 = 6
+const lado2 = 12
+const lado3 = 5
 
-let angulo1;
-let angulo2;
-let angulo3;
-
-if (a + b < c){
+if (lado1 < (lado2 + lado3) && lado2 < (lado1 + lado3) && lado3 < (lado2 + lado1)){
   angulo1=true
 } else {
   angulo1=false
-}
-
-if (a + c < b){
-  angulo2=true
-} else {
-  angulo2=false
-}
-
-if (b + c < c){
-  angulo3=true
-} else {
-  angulo3=false
-}
-
-if ((angulo1 = true) && (angulo2 = true) && (angulo3 = true)){
-  console.log("eres uno tringulinho manito!")
-} else {
-  console.log("no eres uno triangulinho")
 }
