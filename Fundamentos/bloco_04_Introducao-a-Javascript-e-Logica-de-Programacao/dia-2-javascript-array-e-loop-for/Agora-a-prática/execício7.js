9da6e4a8c790b0e1153c3a6fe68f7ee72d6da9d0
pguilhermef <pguilhermesantos@hotmail.com>
2022-05-13 23:49:33 -0300

    Finalização do sétimo exercício.

--- Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/Agora-a-prática/execício7.js
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/Agora-a-prática/execício7.js
@@ -1,11 +1,10 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, -23, 7, -10, 70, 8, 100, -9, 35, 27];
+let menorValor = numbers[0]
 
-menorNumero = 0
-
-for (let index = 0; index < menorNumero.length; index++) {
-  if(menorNumero < numbers[index]){
-    numbers[index] = menorNumero
-  }
+for (let index = 1; index < numbers.length; index++) {
+  if (numbers[index] < menorValor){
+    menorValor = numbers[index]
+  }  
 }
 
-console.log(menorNumero)
+console.log(menorValor)
