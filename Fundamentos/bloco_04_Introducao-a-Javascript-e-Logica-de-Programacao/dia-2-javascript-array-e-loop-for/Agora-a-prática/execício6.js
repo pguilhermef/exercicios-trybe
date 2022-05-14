dfaa63f8b0df339f3c39bf9cfa5c979a676da6ec
pguilhermef <pguilhermesantos@hotmail.com>
2022-05-13 21:50:17 -0300

    Finalização do sexto exercício

--- Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/Agora-a-prática/execício6.js
+++ Fundamentos/bloco_04_Introducao-a-Javascript-e-Logica-de-Programacao/dia-2-javascript-array-e-loop-for/Agora-a-prática/execício6.js
@@ -1,1 +1,15 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [1, 70, 27, 3, 8, 100, 2, 30, 20];
+let impares = []; 
+
+for (let index = 0; index < numbers.length; index++) {
+  if ((numbers[index] %2) === 1){
+    numbers[index] = impares.push(numbers[index])
+  }
+}
+
+if (impares.length === 0) {
+  console.log("nenhum valor ímpar encontrado")
+} else {
+  console.log(impares)
+}
+
